6e140b587fc659d2d33eb50a2f0e79cd49576b84
Christian PAZMANY <PazmanyC@ad.fms-ea.com>
2023-04-20 13:38:03 +0200

    Creation of the branch

--- src/app/component/form-customer/form-customer.component.ts
+++ src/app/component/form-customer/form-customer.component.ts
@@ -6,48 +6,49 @@ import { CartService } from 'src/app/services/cartService/cart.service';
 @Component({
   selector: 'app-form-customer',
   templateUrl: './form-customer.component.html',
-  styleUrls: ['./form-customer.component.css']
+  styleUrls: ['./form-customer.component.css'],
 })
 export class FormCustomerComponent implements OnInit {
-  myForm! : FormGroup;
+  myForm!: FormGroup;
 
-  customer: Customer = new Customer(
-    '',
-    '',
-    '',
-    '',
-    ''
-  );
-constructor(private formBuilder : FormBuilder, private cartService : CartService) {
+  customer: Customer = new Customer('', '', '', '', '');
+  constructor(
+    private formBuilder: FormBuilder,
+    private cartService: CartService
+  ) {}
 
-}
-
-ngOnInit() {
-
-  this.myForm = this.formBuilder.group({
-    name: [this.customer.name,[ Validators.required, Validators.minLength(5)]],
-    firstName: [this.customer.firstName, Validators.required],
-    adress : [this.customer.adress, Validators.required],
-    phone : [this.customer.phone, Validators.required],
-    email : [this.customer.email, Validators.required]
-
-  })
-}
-onSaveCustomer(form: FormGroup)  {
-  console.log(form.errors)
-  if(form.valid){
-    this.cartService.saveCustomer(new Customer(form.value.name, form.value.firstName, form.value.adress, form.value.phone, form.value.email));
+  ngOnInit() {
+    this.myForm = this.formBuilder.group({
+      name: [
+        this.customer.name,
+        [Validators.required, Validators.minLength(5)],
+      ],
+      firstName: [this.customer.firstName, Validators.required],
+      adress: [this.customer.adress, Validators.required],
+      phone: [this.customer.phone, Validators.required],
+      email: [this.customer.email, Validators.required],
+    });
+  }
+  onSaveCustomer(form: FormGroup) {
+    console.log(form.errors);
+    if (form.valid) {
+      this.cartService.saveCustomer(
+        new Customer(
+          form.value.name,
+          form.value.firstName,
+          form.value.adress,
+          form.value.phone,
+          form.value.email
+        )
+      );
+    }
   }
 
-}
-
-get phone() {
-  return this.myForm.get('phone');
-}
-get name() {
-  return this.myForm.get('name');
-}
-getCustomer() {
-
-}
+  get phone() {
+    return this.myForm.get('phone');
+  }
+  get name() {
+    return this.myForm.get('name');
+  }
+  getCustomer() {}
 }
